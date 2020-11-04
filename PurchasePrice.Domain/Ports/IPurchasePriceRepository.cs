0a09d34607d9516393a21baeb2050c88f1f1352d
soyhan-coolblue <soyhan.beyazit@coolblue>
2020-11-04 16:07:54 +0100

    Add domain project and
    the basic repository for managing purchase prices

---
+++ PurchasePrice.Domain/Ports/IPurchasePriceRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using PurchasePrices.Domain.Entities;
+
+namespace PurchasePrices.Domain.Ports
+{
+    public interface IPurchasePriceRepository
+    {
+        Task Save(PurchasePrice purchasePrice);
+        Task Delete(PurchasePrice purchasePrice);
+        Task Update(PurchasePrice purchasePrice);
+        Task<IReadOnlyCollection<PurchasePrice>> GetFor(int supplierId, int productId);
+    }
+}
