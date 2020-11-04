5feaf33de0c1332d6244ff531c46c1465023a5b5
soyhan-coolblue <soyhan.beyazit@coolblue>
2020-11-04 16:11:35 +0100

    Add usecase with tests

---
+++ PurchasePrice.Domain/Usecases/SavePurchasePrice.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+using PurchasePrices.Domain.Entities;
+using PurchasePrices.Domain.Ports;
+
+namespace PurchasePrices.Domain.Usecases
+{
+    public class SavePurchasePrice
+    {
+        private readonly IPurchasePriceRepository _purchasePriceRepository;
+
+        public SavePurchasePrice(IPurchasePriceRepository purchasePriceRepository)
+        {
+            _purchasePriceRepository = purchasePriceRepository;
+        }
+
+        public async Task Invoke(PurchasePrice purchasePrice)
+        {
+            var purchasePrices =
+                await _purchasePriceRepository.GetFor(purchasePrice.SupplierId, purchasePrice.ProductId);
+
+            var upcomingTemporaryPurchasePrice =
+                purchasePrices.SingleOrDefault(a => a.IsUpcoming && a.PriceType == PriceType.Temporary);
+
+            if (upcomingTemporaryPurchasePrice != null)
+                await _purchasePriceRepository.Delete(upcomingTemporaryPurchasePrice);
+
+            await _purchasePriceRepository.Save(purchasePrice);
+        }
+    }
+}
---
+++ PurchasePrices.Tests/WhenSavingPurchasePrice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Moq;
+
+using PurchasePrices.Domain.Entities;
+using PurchasePrices.Domain.Ports;
+using PurchasePrices.Domain.Usecases;
+using PurchasePrices.Tests;
+
+using Xunit;
+
+namespace WhenSavingPurchasePrice
+{
+    public class GivenAValidTemporaryPrice
+    {
+        public class AndNoExistingTemporaryPrice
+        {
+            [Fact]
+            public async Task ShouldSavePurchasePrice()
+            {
+                var purchasePrice = new PurchasePriceBuilder()
+                    .ATemporaryPrice()
+                    .Build();
+
+                var purchasePriceRepositoryMock = new Mock<IPurchasePriceRepository>();
+                purchasePriceRepositoryMock
+                    .Setup(a => a.GetFor(It.IsAny<int>(), It.IsAny<int>()))
+                    .ReturnsAsync(Array.Empty<PurchasePrice>());
+
+                var sut = new SavePurchasePrice(purchasePriceRepositoryMock.Object);
+
+                await sut.Invoke(purchasePrice);
+
+                purchasePriceRepositoryMock.Verify(a => a.Save(purchasePrice), Times.Once);
+
+                purchasePriceRepositoryMock.Verify(a => a.GetFor(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+            }
+        }
+
+        public class AndThereIsUpcomingTemporaryPrice
+        {
+            [Fact]
+            public async Task ShouldDeleteExistingUpcomingTemporaryPriceAndSavePurchasePrice()
+            {
+                var purchasePrice = new PurchasePriceBuilder()
+                    .ATemporaryPrice()
+                    .Build();
+
+                var existingUpcomingPurchasePrice = new PurchasePriceBuilder().ATemporaryPrice()
+                    .WithStartDate(DateTime.Now.AddDays(10))
+                    .WithEndDate(DateTime.Now.AddDays(20))
+                    .Build();
+
+                var purchasePriceRepositoryMock = new Mock<IPurchasePriceRepository>();
+                purchasePriceRepositoryMock
+                    .Setup(a => a.GetFor(It.IsAny<int>(), It.IsAny<int>()))
+                    .ReturnsAsync(new[] { existingUpcomingPurchasePrice });
+
+                var sut = new SavePurchasePrice(purchasePriceRepositoryMock.Object);
+
+                await sut.Invoke(purchasePrice);
+
+                purchasePriceRepositoryMock.Verify(a => a.GetFor(It.IsAny<int>(), It.IsAny<int>()), Times.Once);
+                
+                purchasePriceRepositoryMock.Verify(a => a.Delete(existingUpcomingPurchasePrice), Times.Once);
+
+                purchasePriceRepositoryMock.Verify(a => a.Save(purchasePrice), Times.Once);
+            }
+        }
+    }
+}
